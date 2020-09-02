b19ac6c6ab93bcf7776982c76f2a6b1067d748d3
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-02 13:19:46 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 
 public class Encoder {
 	
-//make arraylist that is table of values
+//make 2D arraylist that is table of values
 	Arraylist<String> table = new ArrayList();
 	
 	//read in file to code
 	BufferedReader br = new BufferedReader(new FileReader(file));
+	
 	
 	
 }
