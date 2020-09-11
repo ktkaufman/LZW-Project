363d2d1b8a0cb5eb0de8b86dd77e572eb5a8c145
nataisha <70167536+nataisha@users.noreply.github.com>
2020-09-11 13:12:03 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-
+//hello
 
 /**
  *	1. read the text until we find. pattern that is not in our table
