b650a0483bf8a07ab3a357c5a2669645833f0518
skylarskylar <70194012+skylarskylar@users.noreply.github.com>
2020-09-11 02:13:00 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -33,7 +33,6 @@ public class Encoder
 	public Encoder () {
 	}
 
-	@SuppressWarnings("unlikely-arg-type")
 	public void encode (String fileName) throws IOException
 	{
 		try {
@@ -64,7 +63,7 @@ public class Encoder
 				}
 
 				else {
-					if ((int)pattern.charAt(0) <= 95) { //checks if it is already in the ascii table
+					if ((int)pattern.charAt(0) <= 128) { //checks if it is already in the ascii table
 						code.add((int)pattern.charAt(0)); //adds the index to the list of codes
 						writer.print((int)pattern.charAt(0)); //prints the code of this pattern
 					}
