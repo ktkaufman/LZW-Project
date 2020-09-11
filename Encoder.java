ecc9d158529e54d83f68bf6506c2e023eb7b2c0a
skylarskylar <70194012+skylarskylar@users.noreply.github.com>
2020-09-10 23:48:52 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -24,28 +24,23 @@ public class Encoder
 	//make a list of strings and their values
 	ArrayList<String> table = new ArrayList<String>();
 
-
-
 	//makes arrayList that stores LZW code
 	ArrayList<Integer> code = new ArrayList<Integer>();
 
 	String text;
-	
+
 	public Encoder (String fileName) {
 		text = fileName;
 	}
 
-
-	
 	public void encodeFile (String inputFileName)
-
 	{
 		//make file reader
 		BufferedReader br = new BufferedReader(new FileReader(inputFileName));
 
 		for (int i=0; i<94; i++)
 		{
-			table.put((char)(i+33)); //inputs values into table
+			table.add(""+(char)(i+33)); //inputs values into table
 		}
 
 		String read = ""; // String that you take in
@@ -58,7 +53,7 @@ public class Encoder
 
 			if (!table.containsKey(read)) //if read is not in table, it adds it to the table
 			{
-				table.put(read);
+				table.add(read);
 				code.add (table.getKey(read.substring(0,read.length()-2))); //adds value of everything but last letter to code
 				read = ""+ read.substring(read.length()-1); //resets with only last char of former sequence
 
@@ -71,17 +66,14 @@ public class Encoder
 
 	}
 
-	
-	public String output () {
+
+	public void generateText(int chainorder, String outputFileName, int numChars) throws IOException
+	{
+		//makes a stringbuffer to loop through table
 		StringBuffer sb = new StringBuffer ("");
 		for (int key = 0; key < table.size(); key++) {
 			sb.append(table.get(key));
 		}
-	}
-
-	public void generateText(int chainorder, String outputFileName, int numChars) throws IOException
-	{
-
 		PrintWriter writer = new PrintWriter(new FileWriter(outputFileName));
 		writer.print(sb);
 
