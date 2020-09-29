4e1c1134a6ba90b113d8d053ee2b433ba9618db1
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-29 16:29:16 -0700

    Update LZWTester3.java

--- LZWTester3.java
+++ LZWTester3.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class LZWTester3 {
 	public static void main (String [] args) throws IOException
 	{
-		Encoder jake = new Encoder();
-		jake.encode("test");
+		//Encoder jake = new Encoder();
+		//jake.encode("test");
 		lzwDecode alsoJake = new lzwDecode();
 		alsoJake.decode("test","testdecoded");
 	}
